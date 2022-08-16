520f278cfe0680d3ea13fbc2b17615f8e029bbaf
Sedat Kurnalı <sedatkurnali@gmail.com>
2022-08-16 23:29:48 +0300

    2022.08.16_v2

--- gitatamailk.bat
+++ gitatamailk.bat
@@ -2,5 +2,7 @@ git init
 git init
 git add .
 git add .
-git commit -m "2022.08.16_v1"
-git commit -m "2022.08.16_v1"
+git commit -m "2022.08.16_v2"
+git commit -m "2022.08.16_v2"
+git branch -M main
+git branch -M main
